ee9caad5fd8123afb40615dceb63db5fca422d94
Leonardo1596 <leonardoemanuel156@gmail.com>
2025-12-25 04:19:05 -0300

    using cors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors'
 import barbershopRoutes from './modules/barbershop/routes';
 import authRoutes from './routes/auth.routes';
 import userRoutes from './modules/users/routes';
@@ -6,6 +7,11 @@ import userRoutes from './modules/users/routes';
 const app = express()
 
 app.use(express.json())
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Tenant-ID'],
+}))
 app.use('/barbershop', barbershopRoutes);
 app.use(authRoutes);
 app.use(userRoutes);
